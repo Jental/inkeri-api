02e87fb832abf1f64299f53b5389d3238fc804dc
Dmitry V. Luciv <dluciv@dluciv.name>
2017-11-30 12:05:46 +0300

    Proper...

--- index.js
+++ index.js
@@ -4,38 +4,51 @@ const PORT = process.env.PORT || 5000
 const request = require('request');
 const unesc = require('unescape');
 
-let ddg_q1 = (qs, callback) => {
+let ddg_q1 = (qs, hd, dbg, callback) => {
   let rrg = /<a class="result__snippet" .+?>(.+?)<\/a>/g;
-  request('https://duckduckgo.com/html?q=' + encodeURIComponent(qs), (error, response, body) => {
-    let m = rrg.exec(body);
-    if(m) {
-      let text = m[1];
-      let plaintext = unesc(text.replace(/<b>(.*?)<\/b>/g, '$1'));
-      callback({'response': plaintext});
-    } else
-      console.log("DDG returned nothing...");
-      console.log(body.substring(0,256));
-      callback({'ddg_body': body});
-  });
-}
+  let headers = { 'User-Agent': hd['user-agent'], 'Accept-Language': hd['accept-language'] }
+  request(
+    {
+      'url': 'https://duckduckgo.com/html?q=' + encodeURIComponent(qs),
+      'headers': headers
+    },
+    (error, response, body) => {
+      let result = {}
+      let m = rrg.exec(body);
+      if(m) {
+        let text = m[1];
+        let plaintext = unesc(text.replace(/<b>(.*?)<\/b>/g, '$1'));
+        result['response'] = plaintext;
+      } 
+      if (!m || dbg) {
+        result['ddg_response'] = response;
+      }
+      callback(result);
+    }
+  );
+};
+
+let setCORS = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+    setCORS(res);
     next();
   })
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .get('/short-thought', (req, res) => {
     let q = req.query.q;
-    ddg_q1(q, (r) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(r));
+    let dbg = req.query.debug === '1';
+    ddg_q1(q, req.headers, dbg, (a) => {
+      res.status(200).json(a);
     });
   })
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
